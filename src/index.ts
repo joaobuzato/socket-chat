e0caf4ccc759740797ba7dbd7bf843cacbef632f
João Pedro Buzato <joaobuzato@gmail.com>
2024-10-04 23:04:28 -0300

    adding connection and cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-// 3. Crie um servidor Express.js.
 import express, { Request, Response } from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import { Readable } from "stream";
+import fs from "fs";
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   path: "/chat",
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"], // Allow GET and POST methods
+  },
+});
+
+let messages: Array<string> = [];
+
+// Load messages from JSON file at startup
+fs.readFile("messages.json", "utf-8", (err, data) => {
+  if (err) {
+    console.error("Failed to load messages from file:", err);
+  } else {
+    messages = JSON.parse(data);
+  }
 });
 
 app.get("/texts", (req: Request, res: Response) => {
@@ -22,6 +40,7 @@ app.get("/texts", (req: Request, res: Response) => {
   });
   stream.pipe(res);
 });
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
@@ -29,11 +48,16 @@ io.on("connection", (socket) => {
   });
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
-  });
-  socket.on("chat message", (msg) => {
+    messages.push(msg);
+    fs.writeFile("messages.json", JSON.stringify(messages), (err) => {
+      if (err) {
+        console.error("Failed to save message:", err);
+      }
+    });
     io.emit("chat message", msg);
   });
 });
+
 httpServer.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
